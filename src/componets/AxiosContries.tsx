265578d706572afca33f5faab0c65c1590ae41c5
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-23 16:39:40 +0600

    Сделал отобродение граничущих стран таким жк как в примере

--- src/componets/AxiosContries.tsx
+++ src/componets/AxiosContries.tsx
@@ -4,7 +4,10 @@ import axios from 'axios';
 interface Country {
     alpha3Code: string;
     name: string;
+    borders: string[];
 }
+
+interface BorderCountry extends Country {}
 
 interface CountriesListProps {
     onSelectCountry: (info: Country) => void;
@@ -12,11 +15,12 @@ interface CountriesListProps {
 
 const CountriesList: React.FC<CountriesListProps> = ({ onSelectCountry }) => {
     const [countries, setCountries] = useState<Country[]>([]);
+    const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
 
     useEffect(() => {
         const fetchCountries = async () => {
             try {
-                const response = await axios.get('https://restcountries.com/v2/all?fields=alpha3Code,name');
+                const response = await axios.get<Country[]>('https://restcountries.com/v2/all?fields=alpha3Code,name,capital,population,borders');
                 setCountries(response.data);
             } catch (error) {
                 console.error('Error fetching countries:', error);
@@ -26,8 +30,31 @@ const CountriesList: React.FC<CountriesListProps> = ({ onSelectCountry }) => {
         fetchCountries();
     }, []);
 
-    const handleCountrySelect = (country: Country) => {
-        onSelectCountry(country);
+    const fetchBorderCountryNames = async (borderCodes: string[]) => {
+        try {
+            const borderCountriesData = await Promise.all(
+                borderCodes.map(async (borderCode) => {
+                    const response = await axios.get<BorderCountry>(`https://restcountries.com/v2/alpha/${borderCode}`);
+                    return response.data.name;
+                })
+            );
+            return borderCountriesData;
+        } catch (error) {
+            console.error('Error fetching border countries:', error);
+            return [];
+        }
+    };
+
+    const handleCountrySelect = async (country: Country) => {
+        setSelectedCountry(country);
+
+        if (country.borders && country.borders.length > 0) {
+            const borderCountryNames = await fetchBorderCountryNames(country.borders);
+            const updatedCountry = { ...country, borders: borderCountryNames };
+            onSelectCountry(updatedCountry);
+        } else {
+            onSelectCountry(country);
+        }
     };
 
     return (
--- src/componets/DisplayCountriesInformation.tsx
+++ src/componets/DisplayCountriesInformation.tsx
@@ -23,8 +23,17 @@ const DisplayInfo: React.FC<DisplayInfoProps> = ({ countryInfo }) => {
                     <h2>{countryInfo.name}</h2>
                     <p>Столица: {countryInfo.capital}</p>
                     <p>Население: {countryInfo.population}</p>
-                    <p>Граничит с: {countryInfo.borders ? countryInfo.borders.join(', ') : 'Нет данных о границах'}</p>
-                    <img src={countryInfo.flag} alt={`${countryInfo.name} флаг`} />
+                    <p>Граничит с: </p>
+                    <ul>
+                        {countryInfo.borders ? countryInfo.borders.map((border, index) => (
+                            <li key={index}>{border}</li>
+                        )) : <li>Нет данных о границах</li>}
+                    </ul>
+                    {countryInfo.flag && countryInfo.flag.svg ? (
+                        <img src={countryInfo.flag.svg} alt={`${countryInfo.name} флаг`} />
+                    ) : (
+                        <p>Нет изображения флага</p>
+                    )}
                 </>
             ) : (
                 <p>Выберите страну</p>
